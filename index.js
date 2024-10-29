d0843f2518cae1e63cd2200fd37c01b98a6a3936
VoyageInfotech <voyageinfotech01@gmail.com>
2024-10-29 10:42:57 +0530

    include login flow

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-  const express = require("express");
+const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const imageRouter = require("./routes/image");
-const ContactRouter = require("./routes/contact");
-const DbConnection = require("./config/db");
-const GetTouchRouter = require("./routes/gettouch");
-const TestimonialRouter = require("./routes/testimonial");
-const TechnologyRouter = require("./routes/technology");
-const ResumeRouter = require("./routes/resume");
-const UserRouter = require("./routes/auth");
+const contactRouter = require("./routes/contact");
+const dbConnection = require("./config/db");
+const getTouchRouter = require("./routes/gettouch");
+const testimonialRouter = require("./routes/testimonial");
+const technologyRouter = require("./routes/technology");
+const resumeRouter = require("./routes/resume");
+const userRouter = require("./routes/auth");
 
 dotenv.config();
-
 const PORT = process.env.PORT || 6000;
-
-DbConnection();
+dbConnection();
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello Voyage Infotech");
 });
 
-// Register routes
 app.use("/api", imageRouter);
-app.use("/api/user", UserRouter);
-app.use("/api/contact", ContactRouter);
-app.use("/api/resume", ResumeRouter);
-app.use("/api/technology", TechnologyRouter);
-app.use("/api/gettouch", GetTouchRouter);
-app.use("/api/testimonial", TestimonialRouter);
+app.use("/api/user", userRouter);
+app.use("/api/contact", contactRouter);
+app.use("/api/resume", resumeRouter);
+app.use("/api/technology", technologyRouter);
+app.use("/api/gettouch", getTouchRouter);
+app.use("/api/testimonial", testimonialRouter);
 
 // Start the server
 const server = app.listen(PORT, (err) => {
